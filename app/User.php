eec64b3d30822c354e40211a148102273ec2b6b4
andre <andre.szlima1@gmail.com>
2020-01-02 16:38:56 -0300

    Seeds

--- app/User.php
+++ app/User.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Model\Question;
+use App\Model\Reply;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -39,6 +41,11 @@ class User extends Authenticatable
 
     public function questions()
     {
-        return $this->hasMany(Question:class);
+        return $this->hasMany(Question::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
     }
 }
