cdbe0555672e6a31235d95152ebed9d9d42d1ca1
andre <andre.szlima1@gmail.com>
2020-01-02 15:43:04 -0300

    Migrations

---
+++ database/migrations/2020_01_02_151205_create_replies_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('body');
+
+            $table->unsignedBigInteger('question_id');
+            $table->unsignedBigInteger('user_id');
+
+
+            $table->timestamps();
+
+            $table->index('question_id');
+            $table->index('user_id');
+
+            $table->foreign('question_id')->references('questions')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
