e5b2b0c756cf913bdc07a5d2b9c79a1bb294a12c
andre <andre.szlima1@gmail.com>
2020-01-05 16:41:44 -0300

    Login with Vue

---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <v-toolbar>
+            <v-toolbar-title>Forum</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <div class="hidden-sm-and-down">
+                <v-btn text>Forum</v-btn>
+                <v-btn text>Ask question</v-btn>
+                <v-btn text>Category</v-btn>
+                <router-link to="/login">
+                    <v-btn text>Login</v-btn>
+                </router-link>
+            </div>
+
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Toolbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/login/Login.vue
@@ -0,0 +1,81 @@
+<template>
+    <v-form @submit.prevent="login">
+        <v-container>
+            <v-row>
+
+                <v-col
+                    cols="4"
+                    md="4"
+                    offset="4"
+                    offset-md="4"
+                >
+                    <v-text-field
+                        v-model="form.email"
+                        label="E-mail"
+                        type="email"
+                        required
+                    ></v-text-field>
+                </v-col>
+
+                <v-col
+                    cols="4"
+                    md="4"
+                    offset="4"
+                    offset-md="4"
+
+                >
+                    <v-text-field
+                        v-model="form.password"
+                        label="Senha"
+                        type="password"
+                        required
+                    ></v-text-field>
+                </v-col>
+
+                <v-col
+                    cols="4"
+                    md="4"
+                    offset="4"
+                    offset-md="4"
+                >
+                    <v-btn
+                        color="purple"
+                        type="submit"
+                    >
+                        Entrar
+                    </v-btn>
+                </v-col>
+
+
+
+            </v-row>
+        </v-container>
+    </v-form>
+</template>
+
+<script>
+
+    const axios = require('axios');
+
+    export default {
+        data(){
+            return {
+                form: {
+                    email:null,
+                    password:null,
+                }
+            }
+        },
+        methods: {
+            login() {
+                this.$http.post('/api/auth/login', this.form)
+                .then(res => console.log(res.data))
+                .catch(error => console.log(error.response.data))
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
